a79a46762a8eb38cb4171e38bc3ea1feb9c63cea
odiroot <odi.root@gmail.com>
2011-11-19 15:57:49 +0100

    Launching script.

---
+++ common_launch.sh
@@ -0,0 +1,12 @@
+DIST_DIR="./assets"
+
+COIL_FILE="${DIST_DIR}/Coil_for_Opt.param"
+GRID_FILE="${DIST_DIR}/Coil_for_Opt.grid"
+OUT_XML="${DIST_DIR}/optimal_coil.xml"
+OUT_CBL="${DIST_DIR}/optimal_coil.cbl"
+STOP_FILE="/tmp/aFlag"
+FINE=7
+NGEN=${NGEN:-10}
+POPSIZE=${POPSIZE:-20}
+SEED=1337
+
--- launch_pyopt_min.sh
+++ launch_pyopt_min.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
-DIST_DIR="../dist/unix"
-. ../common_launch.sh
+. ./common_launch.sh
 
-./optimizer_ga.py \
+./pyopt_min/optimizer_ga.py \
     -coil ${COIL_FILE} \
     -grid ${GRID_FILE} \
     -fine ${FINE} \
     -ngen ${NGEN} \
     -popsize ${POPSIZE} \
     -seed ${SEED} \
+    -xml ${OUT_XML} \
+    -cblock ${OUT_CBL} \
+    -stopflag ${STOP_FILE} \
     $@
 ##
 
