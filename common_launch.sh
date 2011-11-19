967d7078085373bb32b248760b7e1113cac5b8fe
odiroot <odi.root@gmail.com>
2011-11-19 15:59:36 +0100

    Git ignores.

--- common_launch.sh
+++ common_launch.sh
@@ -1,9 +1,10 @@
 DIST_DIR="./assets"
+OUT_DIR="${DIST_DIR}/output"
 
 COIL_FILE="${DIST_DIR}/Coil_for_Opt.param"
 GRID_FILE="${DIST_DIR}/Coil_for_Opt.grid"
-OUT_XML="${DIST_DIR}/optimal_coil.xml"
-OUT_CBL="${DIST_DIR}/optimal_coil.cbl"
+OUT_XML="${OUT_DIR}/optimal_coil.xml"
+OUT_CBL="${OUT_DIR}/optimal_coil.cbl"
 STOP_FILE="/tmp/aFlag"
 FINE=7
 NGEN=${NGEN:-10}
