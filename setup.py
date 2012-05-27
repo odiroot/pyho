6ba41c6b3c6d0eeeecc336c8ba94dbc6ebe28310
odiroot <odi.root@gmail.com>
2012-05-27 21:38:38 +0200

    Support dynamic version number.

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup, find_packages
 from os.path import join, dirname
+import pyho
+
 
 setup(
     name="pyho",
-    version="1.0",
+    version=pyho.__version__,
     packages=find_packages(),
     long_description=open(join(dirname(__file__), "README.txt")).read(),
     entry_points={
