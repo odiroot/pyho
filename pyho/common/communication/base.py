50c59be8b92d74b183e06f7d4a3bed47da4e012a
odiroot <odi.root@gmail.com>
2012-05-30 21:52:13 +0200

    Matched communication protocol to thesis.

--- pyho/common/communication/base.py
+++ pyho/common/communication/base.py
@@ -4,10 +4,10 @@ class MessageTypeError(ValueError):
 
 class MessageType(object):
     GET_OPTIONS = 1
-    RESP_OPTIONS = 2
-    EVALUATE = 3
-    RESP_SCORE = 4
-    GET_STATS = 5
-    RESP_STATS = 6
-    SAVE_OUTPUT = 7
-    RESP_SAVE = 8
+    RESP_OPTIONS = 101
+    EVALUATE = 2
+    RESP_SCORE = 102
+    GET_STATS = 3
+    RESP_STATS = 103
+    SAVE_OUTPUT = 4
+    RESP_SAVE = 104
