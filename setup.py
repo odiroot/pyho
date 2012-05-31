bfddec79639cc5e0b00f413ffa3faedea6d37210
odiroot <odi.root@gmail.com>
2012-05-31 23:12:32 +0200

    Author and license info.

--- setup.py
+++ setup.py
@@ -30,6 +30,8 @@ setup(
     author_email="odnousm@iem.pw.edu.pl",
     description="Hybrid optimization system.",
     version=pyho.__version__,
+    url="https://bitbucket.org/odiroot/pyho",
+    license="BSD (optimizer code only)",
 
     dependency_links=[
         "http://pyevolve.sourceforge.net/distribution/0_6rc1/"
